b4ce597f017b9d925f9ad40238fa919a68f7d791
thammi <devnull@localhost>
2010-08-18 02:06:18 +0200

    Starting xmms implementation

---
+++ src/xmms.py
@@ -0,0 +1,37 @@
+from xmmsclient import XMMS, userconfdir_get
+from xmmsclient.glib import GLibConnector
+from os.path import join
+
+# TODO: think of a cool name
+_ID = 'INSERT_NAME_HERE'
+
+def get_config_dir():
+    return join(osuserconfdir_get(), "clients", _ID)
+
+class XmmsConnection:
+
+    def __init__(self):
+        self.connected = False
+
+        xmms = self.xmms = XMMS(_ID)
+
+        self.connect()
+
+    def connect(self):
+        if not self.connected:
+            def disconnected(r):
+                self.connected = False
+
+            try:
+                xmms = self.xmms
+
+                # TODO: use path conventions
+                xmms.connect(disconnect_func=disconnected)
+                GLibConnector(xmms)
+            except IOError:
+                self.connected = False
+            else:
+                self.connected = True
+
+        return self.connected
+
