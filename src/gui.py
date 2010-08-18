7d167c13d1dc24a6316c72ecbd4de06e1d76050f
thammi <devnull@localhost>
2010-08-18 02:12:56 +0200

    Adding a virtual status bar to the design

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,12 @@
 import urwid
 
+# program status
+f_status = urwid.SolidFill('S')
+status = urwid.BoxAdapter(f_status, 1)
+
 # current media status
-f_status = urwid.SolidFill('C')
-status = urwid.BoxAdapter(f_status, 3)
+f_current = urwid.SolidFill('C')
+current = urwid.BoxAdapter(f_status, 3)
 
 # media browser
 browser = urwid.SolidFill('B')
@@ -12,7 +16,7 @@ playlist = urwid.SolidFill('P')
 
 split = urwid.Columns([browser, playlist])
 
-frame = urwid.Frame(split, status)
+frame = urwid.Frame(split, current, status)
 
 loop = urwid.MainLoop(frame, event_loop=urwid.GLibEventLoop())
 loop.run()
