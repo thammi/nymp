dd395b13777f5a71a7a3420271cddf64e0af885d
thammi <devnull@localhost>
2010-08-21 01:04:29 +0200

    XMMS: adding the player module and event support

--- src/events.py
+++ src/events.py
@@ -1,5 +1,11 @@
 class NoEventException(Exception):
-    pass
+
+    def __init__(self, event_id, listener):
+        self.event_id = event_id
+        self.listener = listener
+
+    def __str__(self):
+        return "NoEventException: %s for %s" % (self.listener, self.event_id)
 
 # TODO: use glib?
 class EventEmitter:
@@ -8,24 +14,27 @@ class EventEmitter:
         self.strict = demand_registration
         self.listeners = {}
 
-    def emit(event_id, *args):
+    def emit(self, event_id, *args):
         listeners = self.listeners
 
         if event_id in listeners:
             for listener in listeners[event_id]:
                 listener(*args)
 
-    def listen(event_id, listener):
+    def listen(self, event_id, listener):
         listeners = self.listeners
 
         if event_id not in listeners:
             if self.strict:
-                raise NoEventException()
+                raise NoEventException(event_id, listener)
             else:
                 register(event_id)
 
         listeners[event_id].append(listener)
 
-    def register(event_id):
-        listeners[event_id] = []
+    def register(self, event_id):
+        listeners = self.listeners
+
+        if event_id not in listeners:
+            listeners[event_id] = []
 
--- src/xmms.py
+++ src/xmms.py
@@ -2,24 +2,42 @@ from xmmsclient import XMMS, userconfdir_get
 from xmmsclient.glib import GLibConnector
 from os.path import join
 
+from events import EventEmitter
+
 # TODO: think of a cool name
 _ID = 'INSERT_NAME_HERE'
 
 def get_config_dir():
     return join(userconfdir_get(), "clients", _ID)
 
-class XmmsConnection:
+
+class XmmsConnection(EventEmitter):
+
+    CONNECT_EVENT = "connect"
+    DISCONNECT_EVENT = "disconnect"
 
     def __init__(self):
+        EventEmitter.__init__(self)
+
         self.connected = False
         self.xmms = None
+
+        # create events
+        self.register(self.CONNECT_EVENT)
+        self.register(self.DISCONNECT_EVENT)
+
+        # initialize modules
+        self.player = Player(self)
 
         self.connect()
 
     def connect(self):
         if not self.connected:
+            # disconnection callback
             def disconnected(r):
                 self.connected = False
+                self.xmms = None
+                self.emit(self.DISCONNECT_EVENT)
 
             try:
                 xmms = self.xmms = XMMS(_ID)
@@ -27,13 +45,52 @@ class XmmsConnection:
                 # TODO: use path conventions
                 xmms.connect(disconnect_func=disconnected)
                 GLibConnector(xmms)
-
-                xmms.broadcast_playback_status(p)
             except IOError:
                 self.xmms = None
                 self.connected = False
             else:
                 self.connected = True
+                self.emit(self.CONNECT_EVENT)
 
         return self.connected
+
+
+class Player(EventEmitter):
+
+    STATUS_EVENT = "status_changed"
+    VOLUME_EVENT = "volume_changed"
+    CURRENT_EVENT = "current_changed"
+
+    def __init__(self, connection):
+        EventEmitter.__init__(self)
+
+        self.connection = connection
+
+        connection.listen(connection.CONNECT_EVENT, self._connected)
+
+    def _connected(self):
+        xmms = self.connection.xmms
+
+        # set callbacks
+        xmms.broadcast_playback_current_id(self._current_change)
+        xmms.broadcast_playback_status(self._status_change)
+        xmms.broadcast_playback_volume_changed(self._volume_change)
+
+        # get current data
+        xmms.playback_current_id(cb=self._current_change)
+        xmms.playback_status(cb=self._status_change)
+        xmms.playback_volume_get(cb=self._volume_change)
+
+    def _current_change(self, value):
+        xmms = self.connection.xmms
+        xmms.medialib_get_info(value.value(), cb=self._current_info)
+
+    def _current_info(self, result):
+        self.emit(self.CURRENT_EVENT, result.value())
+
+    def _status_change(self, value):
+        self.emit(self.STATUS_EVENT, value.value())
+
+    def _volume_change(self, value):
+        self.emit(self.VOLUME_EVENT, value.value())
 
