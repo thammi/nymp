48275cefccaf9dca09c1dc2354e5ec5846e713ff
thammi <devnull@localhost>
2010-08-22 01:44:08 +0200

    gui/browser: Playing around with focus

--- src/gui/browser.py
+++ src/gui/browser.py
@@ -6,6 +6,11 @@ class BrowserWidget(urwid.ListBox):
         self.xc = xc
 
         l = [urwid.Text('+ item %i'%i) for i in range(256)]
+
+        for t in l:
+            t.keypress = lambda s, k: k
+            t._selectable = True
+
         self.walker = walker = urwid.SimpleListWalker(l)
 
         urwid.ListBox.__init__(self, walker)
