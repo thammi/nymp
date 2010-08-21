4f9faca54c39097bc9f00a504486cf4a91a856c0
thammi <devnull@localhost>
2010-08-21 16:46:32 +0200

    GUI: Modularization and CurrentWidget

--- src/gui.py
+++ src/gui.py
@@ -1,23 +1,67 @@
 import urwid
+import xmms
 
-# program status
-f_status = urwid.SolidFill('S')
-status = urwid.BoxAdapter(f_status, 1)
+class CurrentWidget(urwid.Pile):
 
-# current media status
-f_current = urwid.SolidFill('C')
-current = urwid.BoxAdapter(f_status, 3)
+    def __init__(self, xc):
+        title = self.title = urwid.Text("", 'right')
+        album = self.album = urwid.Text("", 'right')
+        artist = self.artist = urwid.Text("", 'right')
 
-# media browser
-browser = urwid.SolidFill('B')
+        urwid.Pile.__init__(self, [title, album, artist])
 
-# playlist
-playlist = urwid.SolidFill('P')
+        player = xc.player
+        player.listen(player.CURRENT_EVENT, self._update)
 
-split = urwid.Columns([browser, playlist])
+    def _update(self, meta):
+        rm = xmms.reduce_meta(meta)
+        
+        self.title.set_text("%i. %s" % (rm['tracknr'], rm['title']))
+        self.album.set_text(rm['album'])
+        self.artist.set_text(rm['artist'])
 
-frame = urwid.Frame(split, current, status)
+        # TODO: ugly!
+        loop.draw_screen()
 
+
+class BaseWidget(urwid.Frame):
+
+    def __init__(self):
+        xc = self.xc = xmms.XmmsConnection()
+
+        # program status
+        f_status = urwid.SolidFill('S')
+        status = urwid.BoxAdapter(f_status, 1)
+
+        # current media status
+        current = CurrentWidget(xc)
+
+        # media browser
+        browser = urwid.SolidFill('B')
+
+        # playlist
+        playlist = urwid.SolidFill('P')
+
+        split = urwid.Columns([browser, playlist])
+
+        urwid.Frame.__init__(self, split, current, status)
+
+        xc.connect()
+
+    def keypress(self, size, inp):
+        if urwid.Frame.keypress(self, size, inp):
+            xc = self.xc
+
+            hotkeys = {
+                    'p': lambda: xc.player.toggle(),
+                    'n': lambda: xc.player.forward(),
+                    }
+
+            if inp in hotkeys:
+                hotkeys[inp]()
+
+
+frame = BaseWidget()
 loop = urwid.MainLoop(frame, event_loop=urwid.GLibEventLoop())
 loop.run()
 
