57211c2ef895b9d5c3c353d5b8b45141adb6e8f6
thammi <devnull@localhost>
2010-09-06 21:04:25 +0200

    Fixing old reference in license notice

--- src/gui/browser.py
+++ src/gui/browser.py
@@ -3,18 +3,18 @@
 ##  nymp - a graphical xmms2 cli frontend
 ##  Copyright 2010 Thammi
 ##
-##  pymucl is free software: you can redistribute it and/or modify
+##  nymp is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU General Public License as published by
 ##  the Free Software Foundation, either version 3 of the License, or
 ##  (at your option) any later version.
 ##
-##  pymucl is distributed in the hope that it will be useful,
+##  nymp is distributed in the hope that it will be useful,
 ##  but WITHOUT ANY WARRANTY; without even the implied warranty of
 ##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ##  GNU General Public License for more details.
 ##
 ##  You should have received a copy of the GNU General Public License
-##  along with pymucl.  If not, see <http://www.gnu.org/licenses/>.
+##  along with nymp.  If not, see <http://www.gnu.org/licenses/>.
 ##
 ##############################################################################
 
