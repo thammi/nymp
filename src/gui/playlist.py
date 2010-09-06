6e37ea9968789bd91c95cb26dbbf72f635b5bb6e
thammi <devnull@localhost>
2010-09-06 07:55:12 +0200

    Displaying playlist with media id works

--- src/gui/playlist.py
+++ src/gui/playlist.py
@@ -11,16 +11,16 @@ class PlaylistItem:
         self.requested = False
 
     def request(self, xc):
-        raise notimplementederror
+        raise NotImplementedError
 
     def delete(self, xc):
-        raise notimplementederror
+        raise NotImplementedError
 
     def move_up(self, xc):
-        raise notimplementederror
+        raise NotImplementedError
 
     def move_down(self, xc):
-        raise notimplementederror
+        raise NotImplementedError
 
 class CurPlaylistWalker(urwid.ListWalker):
 
@@ -30,6 +30,7 @@ class CurPlaylistWalker(urwid.ListWalker):
         self.playlist = []
         self.cur = None
         self.position = None
+        self._focus = 0
 
         playlist = xc.playlist
         playlist.listen(playlist.LOAD_EVENT, self._playlist_loaded)
@@ -41,38 +42,73 @@ class CurPlaylistWalker(urwid.ListWalker):
         xc.listen(xc.CONNECT_EVENT, self._connect)
 
     def _connect(self):
-        pass
+        self.xc.playlist.current_playlist(self._playlist_loaded)
 
     def _playlist_loaded(self, name):
+        # save new name
         self.cur = name
-        self.xc.playlist.list_entries(name, self._playlist_data_cb)
+
+        # reset to sane defaults while waiting
+        self.playlist = []
+        self.position = None
+        self._focus = 0
+
+        # request data
+        self.xc.playlist.list_entries(name, self._entries_cb)
         self.xc.playlist.position(name, self._position_changed)
 
     def _playlist_changed(self, event):
+        # TODO: implement!
+        # watch out for focus and position when 
         pass
 
     def _position_changed(self, position):
-        self.position = position
-        self.modified()
+        if position['name'] == self.cur:
+            self.position = position['position']
+            self.modified()
 
     def _entries_cb(self, data):
-        print data
+        self.playlist = [PlaylistItem(mid) for mid in data]
     
     def modified(self):
         self._modified()
         update()
+    
+    def _get_widget(self, pos):
+        item = self.playlist[pos]
+        text = SelectableText(unicode(item.media_id))
+        
+        if self.position == pos:
+            return urwid.AttrMap(text, 'current', 'current_focus')
+        else:
+            return urwid.AttrMap(text, 'normal', 'focus')
 
     def get_focus(self):
-        return (None, None)
+        if len(self.playlist):
+            focus = self._focus
+            return (self._get_widget(focus), focus)
+        else:
+            return (None, None)
     
     def get_next(self, pos):
-        return (None, None)
+        if pos < len(self.playlist) - 1:
+            new_pos = pos + 1
+            widget = self._get_widget(new_pos)
+            return (widget, new_pos)
+        else:
+            return (None, None)
 
-    def get_pref(self, pos):
-        return (None, None)
+    def get_prev(self, pos):
+        if pos > 0:
+            new_pos = pos - 1
+            widget = self._get_widget(new_pos)
+            return (widget, new_pos)
+        else:
+            return (None, None)
 
     def set_focus(self, pos):
         self._focus = pos
+        self._modified()
 
 class Playlist(urwid.ListBox):
 
