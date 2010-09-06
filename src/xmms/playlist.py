ca5f0b30eabdcdf02c0c01716c411d2eae641ffb
thammi <devnull@localhost>
2010-09-06 06:54:29 +0200

    Wrapping much of the playlist api of xmms2

--- src/xmms/playlist.py
+++ src/xmms/playlist.py
@@ -1,13 +1,19 @@
 from events import EventEmitter
+from helper import value_wrap
 
 # TODO: finish
 class Playlist(EventEmitter):
     """Everything considering the playlist"""
 
     CHANGE_EVENT = "playlist_changed"
+    LOAD_EVENT = "playlist_loaded"
+    POSITION_EVENT = "playlist_loaded"
 
     def __init__(self, connection):
         EventEmitter.__init__(self)
+        
+        self.register(self.CHANGE_EVENT)
+        self.register(self.LOAD_EVENT)
 
         self.connection = connection
 
@@ -17,18 +23,56 @@ class Playlist(EventEmitter):
 
         connection.listen(connection.CONNECT_EVENT, self._connected)
 
+    def add_collection(self, coll, order, playlist=None, cb=None):
+        xmms = self.connection.xmms
+        xmms.playlist_add_collection(coll, order, playlist, value_wrap(cb))
+
+    def clear(self, playlist=None, cb=None):
+        xmms = self.connection.xmms
+        xmms.playlist_clear(coll, order, playlist, value_wrap(cb))
+
+    def current_playlist(self, cb):
+        xmms = self.connection.xmms
+        xmms.playlist_current_active(value_wrap(cb))
+
+    def position(self, playlist, cb):
+        xmms = self.connection.xmms
+        xmms.playlist_current_pos(playlist, value_wrap(cb))
+
+    def list_entries(self, playlist, cb):
+        xmms = self.connection.xmms
+        xmms.playlist_list_entries(playlist, value_wrap(cb))
+
+    def list_playlists(self, cb):
+        xmms = self.connection.xmms
+        xmms.playlist_list(value_wrap(cb))
+
+    def move_entry(self, old, new, playlist=None, cb=None):
+        xmms = self.connection.xmms
+        xmms.playlist_move(old, new, playlist, value_wrap(cb))
+
+    def move_entry(self, media_id, playlist=None, cb=None):
+        xmms = self.connection.xmms
+        xmms.playlist_remove_entry(old, new, playlist, value_wrap(cb))
+
+    def get_info(self, media_id, cb):
+        xmms = self.connection.xmms
+        xmms.medialib_get_info(media_id, value_wrap(cb))
+
     def _connected(self):
         xmms = self.connection.xmms
 
         # set callbacks
         xmms.broadcast_playlist_changed(self._change)
         xmms.broadcast_playlist_loaded(self._loaded)
+        xmms.broadcast_playlist_current_pos(self._position)
+
+    def _position(self, value):
+        self.emit(self.POSITION_EVENT, value.value())
 
     def _change(self, value):
-        pass
-        #print value.value()
+        self.emit(self.CHANGE_EVENT, value.value())
 
     def _loaded(self, value):
-        pass
-        #print value.value()
+        self.emit(self.LOAD_EVENT, value.value())
 
