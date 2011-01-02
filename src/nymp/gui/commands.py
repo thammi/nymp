623c733dc70b35e18a1f7ea5bad15c43201d66fc
Thammi <thammi@chaossource.net>
2011-01-02 07:50:41 +0100

    Adding default key bindings

---
+++ src/nymp/gui/commands.py
@@ -0,0 +1,29 @@
+commands = {
+        # common
+        'activate': ['enter'],
+        'nav_up': ['k', 'up'],
+        'nav_down': ['j', 'down'],
+        # global
+        'col_swap': ['tab'],
+        'col_left': ['h'],
+        'col_right': ['l'],
+        'clear': ['C'],
+        'play': ['p'],
+        'stop': ['c'],
+        'next': ['n'],
+        'prev': ['b'],
+        'quit': ['q'],
+        'vol_down': ['9'],
+        'vol_up': ['0'],
+        # browser
+        'add': ['a'],
+        'expand': ['right'],
+        'fold': ['left'],
+        # playlist
+        'delete': ['d'],
+        'mark': [' '],
+        'reset_mark': ['meta  '],
+        'move_up': ['K'],
+        'move_down': ['J'],
+        }
+
